ed8545f9764ede2007e4373e30e66229090ec500
Kevin Cieszkowski <kevinjc@gmx.com>
2013-07-02 22:03:51 -0700

    Initial RoR Project Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trendssnap::Application.config.secret_key_base = 'a7ea99129631e885aa41883f7cc2d12a6fa3bf4b14dbac5fc0bb15139cdbd6997c56ea0dab0b47045438a10667fa587198be3e029023ceb55796b9c70a11be0f'
